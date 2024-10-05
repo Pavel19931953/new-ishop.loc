72b8ac9fbc01a2be645027cd66acf7a268c52681
Павло Миронічев <pavlomironicev@Air-Pavlo.fritz.box>
2024-10-05 12:36:37 +0200

    update2

--- app/views/layouts/ishop.php
+++ app/views/layouts/ishop.php
@@ -137,10 +137,10 @@ use wfm\View;
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link" href="category.html">Компьютер</a>
+                                <a class="nav-link" href="category.html">Компьютерs</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="category.html">Планшетики</a>
+                                <a class="nav-link" href="category.html">Планшетикs</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
